2d14be2db87be011a2aee6fe4f62faf4cc894aac
Benjamin Jones <bljones36@gmail.com>
2018-09-28 11:25:47 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer
+  attr_accessor :filename, :path
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+   entries = Dir.entries(@path).select {|f| !File.directory? f}
+  end
+  
+  def import
+    self.files
+    Song.new_by_filename(filename)
+  end
+end
